675472473ec4975a3e4dedd8e45415704df7230a
jck-y <s2200582@student.unklab.ac.id>
2024-09-03 23:46:04 +0800

    admin login

--- src/screens/LoginWill/index.js
+++ src/screens/LoginWill/index.js
@@ -1,26 +1,48 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import {AuthContext} from '../../../src/context/AuthContext';
 
-const LoginWill = () => {
-  const [username, setUsername] = useState('');
+const LoginWill = ({navigation}) => {
+  const {mpUser} = useContext(AuthContext);
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    if (username === 'user' && password === 'password') {
-      Alert.alert('Login Successful', `Welcome, ${username}!`);
-    } else {
-      Alert.alert('Login Failed', 'Incorrect username or password.');
+  const handleAdminLogin = async () => {
+    try {
+      const userCredential = await auth().signInWithEmailAndPassword(
+        email,
+        password,
+      );
+      const user = userCredential.user;
+      if (user.email === 'pedagangwil@gmail.com') {
+        console.log('Admin logged in:', user);
+        console.log('Navigation:', navigation);
+        navigation.navigate('PrivateRoutes', {screen: 'HomeTab'});
+      } else {
+        Alert.alert('Access Denied', 'You do not have admin privileges.');
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Login failure', error.message);
     }
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+      <Text style={styles.title}>Admin Login</Text>
       <TextInput
         style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
       />
       <TextInput
         style={styles.input}
@@ -29,7 +51,11 @@ const LoginWill = () => {
         onChangeText={setPassword}
         secureTextEntry
       />
-      <Button title="Login" onPress={handleLogin} />
+      <TouchableOpacity activeOpacity={0.5} onPress={handleAdminLogin}>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.textStyle}>Login</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -39,6 +65,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 20,
+    backgroundColor: '#3CD3E4',
   },
   title: {
     fontSize: 24,
@@ -53,6 +80,21 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 10,
+    backgroundColor: '#F7F2F2',
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    width: 280,
+    height: 39,
+    backgroundColor: '#13274A',
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  textStyle: {
+    fontSize: 15,
+    color: 'white',
   },
 });
 
